0b100d82029b64a2c15f10ccffc26c41c626549c
Ryan Yurkanin <ryurkanin@getguru.com>
2018-04-13 10:08:14 -0400

    Repo stylings, Google Analytics

--- src/modules/Repos/Repo/index.js
+++ src/modules/Repos/Repo/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Star from 'react-icons/lib/go/star';
 
 import './Repo.css';
 
 export default class Repo extends Component {
   render() {
-    const { title, description, link } = this.props;
+    const { title, description, link, stars } = this.props;
 
     return (
       <div className="Repo">
@@ -13,6 +14,14 @@ export default class Repo extends Component {
           {title}
         </a>
         <div className="Repo__description">{description}</div>
+        {!!stars && (
+          <div className="Repo__meta">
+            <div className="Repo__meta-item">
+              <Star className="Repo__meta-item-icon" size="0.8rem" />
+              <span className="Repo__meta-item-text">{stars}</span>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
@@ -22,4 +31,9 @@ Repo.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
+  stars: PropTypes.number,
+};
+
+Repo.defaultProps = {
+  stars: 0,
 };
